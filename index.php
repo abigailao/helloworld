37c963f9859d104b27d13ef29dc0e733138ef1cc
abigailao <abigail.ao@outlook.com>
2018-04-23 14:21:22 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Abigail
+ * Date: 23-4-2018
+ * Time: 09:31
+ */
+$aantal = 14;
+$deze_jongen = "Deze jongen is ";
+
+echo $deze_jongen.$aantal. " jaar oud!";
