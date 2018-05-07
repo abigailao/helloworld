3f2cc8263acf59f2631c30985b131b67bc0bc959
abigailao <abigail.ao@outlook.com>
2018-05-07 12:07:22 +0200

    002 - Beginnen met Programmeren in PHP

--- index.php
+++ index.php
@@ -2,10 +2,51 @@
 /**
  * Created by PhpStorm.
  * User: Abigail
- * Date: 23-4-2018
- * Time: 09:31
- */
-$aantal = 14;
-$deze_jongen = "Deze jongen is ";
+ * Date: 23-4-*/
+/*002 - Beginnen met Programmeren in PHP*/
+//-----------------------------------------------------
 
-echo $deze_jongen.$aantal. " jaar oud!";
+//$aantal = 14;
+//$deze_jongen = "Deze jongen is ";
+
+//echo $deze_jongen.$aantal. " jaar oud!";
+//-----------------------------------------------------
+//$a = "Ik heb niet gefaald.";
+//$b = "die niet werken.";
+//$c = 10000;
+//$d = " Ik heb alleen ";
+//$e = " manier gevonden ";
+//
+//echo $a.$d.$c.$e.$b.'<br>'.
+//"-Thomas Edison";
+//-----------------------------------------------------
+//$a = "die geen fout";
+//$b = "en ";
+//$c = "niet.";
+//$d = "Mensen ";
+//$e = "maken, werken ";
+//$f = "-Albert Plesman (KLM)";
+//
+//echo $d.$a.$b.$e.$c.'<br>'.$f;
+//-----------------------------------------------------
+//$a = "ry";
+//$b = ", but";
+//$c = "fail";
+//$d = "T";
+//$e = " ";
+//$f = "and ";
+//$g = " never fail to try.";
+//$h = "-Tyra Banks";
+//
+//echo $d.$a.$e.$f.$c.$b.$g.'<br>'.$h;
+//-----------------------------------------------------
+$a = "\"omgaan";
+$b = "met";
+$c = "teleu";
+$d = " ";
+$e = "De cursus ";
+$f = " met ";
+$g = "rstellingen\" kan vanavond helaas niet doorgaan.";
+$h = "- Herman Finkers";
+
+echo $e.$a.$d.$f.$c.$g.'<br>'.$h;
