e9707ea87d2b662119827da2246c9be3bbfa3d16
abigailao <abigail.ao@outlook.com>
2018-05-21 21:08:26 +0200

    wisteria

---
+++ 003.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Opdrachten</title>
+    <style>
+
+    </style>
+</head>
+<body>
+<?php
+// for($i = 1; $i<=9 ;$i++) {
+//     echo "<img src='img/binturongs/Binturong".$i.".jpg'>";
+// }
+$wisterias = array("wisteria1.jpg", "wisteria2.jpg", "wisteria3.jpg");
+
+foreach($wisterias as $wisteria) {
+    echo "<img src='img/wisterias/".$wisteria."'>";
+}
+
+?>
+</body>
+</html>
